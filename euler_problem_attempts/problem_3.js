a804b17af64f4d0d6039cc071db83f8a78ce2ff2
55555 55555 <home@55555s-MacBook-Pro.local>
2016-07-22 15:22:02 -0400

    Euler problem 3 attempt

---
+++ euler_problem_attempts/problem_3.js
@@ -0,0 +1,24 @@
+var numb = 600851475143;
+var highestFactor = 0;
+var upto = Math.sqrt(numb);
+
+
+for (var fact = 2; fact < upto; fact++){
+
+  isPrime = true;
+
+    if((numb % fact) === 0) {
+
+       for(var factTwo = 2; factTwo < Math.sqrt(fact); factTwo++){
+          if (fact % factTwo === 0) {
+            isPrime = false;
+            break;
+        }
+       }
+       if(isPrime) {
+         highestFactor = fact;
+       }
+}
+};
+
+console.log(highestFactor);
