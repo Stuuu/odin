1b51603360b367bdd3221526f57b19fe7c858723
Stuart Sch <home@55555s-MacBook-Pro.local>
2016-07-31 04:52:16 -0400

    practicing Ruby

---
+++ ruby/calc.rb
@@ -0,0 +1,9 @@
+hours_in_a_year = 365 * 24
+
+minutes_in_a_decade =(hours_in_a_year * 60) * 10
+
+seconds_old = (minutes_in_a_decade * 60) * 2.67
+
+puts "There are #{hours_in_a_year} hours in a year."
+puts "There are #{minutes_in_a_decade} mins in a decade."
+puts "I am #{seconds_old} seconds old."
