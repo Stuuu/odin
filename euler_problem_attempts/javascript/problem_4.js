9f78832fa6892dc53bc6641cfdb8b50a5eabc652
Stuart Sch <stuartschrick@gmail.com>
2016-08-21 02:51:29 -0400

    Removed divisor check

--- euler_problem_attempts/javascript/problem_4.js
+++ euler_problem_attempts/javascript/problem_4.js
@@ -3,15 +3,15 @@ for (i=999; i > 99; i--){
   for (f=999;f!=i; f--){
   var num = (f * i);
   // #explored pre-checking last digit for "palindromic" compatibility to decrease iterations
-  var lastDigit = num.toString().slice(-1);
-  var firstDigit = num.toString()[0];
-  if (firstDigit === lastDigit){
-  var f_int = num.toString();
-  var r_int = f_int.split("").reverse().join("");
-  if ( f_int === r_int) {
-      palys.push(num);
-    }
-  }
+        var lastDigit = num.toString().slice(-1);
+        var firstDigit = num.toString()[0];
+        if (firstDigit === lastDigit){
+        var f_int = num.toString();
+        var r_int = f_int.split("").reverse().join("");
+        if ( f_int === r_int) {
+            palys.push(num);
+          }
+      }
 }
 };
 var uniq_pals = palys.reduce(function(a,b){
