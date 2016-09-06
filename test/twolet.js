85cbacb56a180582a0a1b5992c51e9b84b665639
Stuart Sch <stuartschrick@gmail.com>
2016-09-05 23:32:49 -0400

    loop test

---
+++ test/twolet.js
@@ -0,0 +1,8 @@
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+for(i=0; i <= alpha.length; i++){
+  for(l=0; l <= alpha.length; l++){
+    console.log(alpha[i] + alpha[l] );
+}
+};
